9a3f4db6c3fa71aaabd971723170e3d0dcbcdceb
Jorj Hambaryan <jorjhambaryan@Jorjs-MBP.home>
2024-07-01 11:03:09 +0400

    fix

--- back/src/db.ts
+++ back/src/db.ts
@@ -7,7 +7,10 @@ const MONGO_URI = process.env.MONGO_URI || '';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, // Increase server selection timeout
+      socketTimeoutMS: 45000, // Increase socket timeout
+    });
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('Failed to connect to MongoDB', err);
